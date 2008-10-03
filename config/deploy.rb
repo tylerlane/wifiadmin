dd159e30dafbec64d35c4e933a5d59a530a94c0c
Tyler Lane <tyler@nolongervalid.com>
2008-10-03 13:21:26 -0500

    css and layout tweaks

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,19 +5,6 @@ set :scm_verbose, true
 set :branch, "master" 
 set :deploy_via, :remote_cache
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
-
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
-
-#role :app, "your app-server here"
-#role :web, "your web-server here"
-#role :db,  "your db-server here", :primary => true
-
 set :application, "wifiadmin" 
 set :deploy_to, "/opt/rails/#{application}" 
 set :user, "root" 
